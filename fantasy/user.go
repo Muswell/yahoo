beb0c703c324160099e2923770dfdbbfddcfe550
Shane Scanlon <shane.scanlon@luz.com>
2016-02-04 20:03:23 -0800

    Initial commit

---
+++ fantasy/user.go
@@ -0,0 +1,8 @@
+// Fantasy wraps the Yahoo fantasy API into Go types and provides functions to generate proper request URLs.
+package fantasy
+
+// User type represents a single Yahoo fantasy user
+type User struct {
+	// Guid is the unique ID of the user
+	Guid string
+}
