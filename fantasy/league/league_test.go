42b8c19d63366e44b6dbf98fc4bac505962ef39b
Shane Scanlon <slickrickjamesbrown@gmail.com>
2016-02-21 20:24:03 -0800

    Test League Basic and User Query

--- fantasy/league/league_test.go
+++ fantasy/league/league_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestGameQueryBuilderURL(t *testing.T) {
@@ -54,37 +55,103 @@ func TestGameQueryBuilderURL(t *testing.T) {
 		}
 	}
 }
-
-func TestQuerySingleLeagueMetaData(t *testing.T) {
-	q := QueryBuilder{Keys: []string{"357.l.86753"}}
+func TestQueryLeaugeErrors(t *testing.T) {
+	qb := QueryBuilder{Keys: []string{"abc"}}
 	client := gotest.NewRegisteredClient()
+	url := qb.Url()
+
+	// test bad client request
+	_, err := qb.Get(client.Client)
+	if err == nil {
+		t.Error("Expected QueryBuilder.Get to return an error")
+	}
+
+	// test non-xml response
+	client.Register(url, "get", gotest.NewSimpleRoundTrip([]byte("Hello, world"), nil))
+	_, err = qb.Get(client.Client)
+	if err == nil {
+		t.Error("Expected QueryBuilder.Get to return an error")
+	}
+}
+
+type queryTest struct {
+	qb     *QueryBuilder
+	client *gotest.RegisteredClient
+	want   int
+	next   func([]League, *testing.T)
+}
+
+func TestLeagueQueries(t *testing.T) {
+	var tests = []queryTest{
+		getSingleMetaTestSet(t),
+		getUserSingleMetaTestSet(t),
+	}
+
+	for _, test := range tests {
+
+		leagues, err := test.qb.Get(test.client.Client)
+		if err != nil {
+			t.Errorf("Unexpected QueryBuilder.Get error: %s", err)
+		}
+		if len(leagues) != test.want {
+			t.Errorf("Unexpected League len got %d, expected %d", len(leagues), test.want)
+		}
+
+		if test.next != nil {
+			test.next(leagues, t)
+		}
+	}
+}
+
+func getSingleMetaTestSet(t *testing.T) queryTest {
+	q := QueryBuilder{Keys: []string{"357.l.86753"}}
 	url := q.Url()
 
+	return queryTest{
+		qb:     &q,
+		client: getXMLClient(url, "single-league-meta.xml", t),
+		want:   1,
+		next: func(l []League, t *testing.T) {
+			league := l[0]
+
+			if league.GameCode != "mlb" {
+				t.Errorf("League unmarshaled incorrectley. GameCode: %s, expected %s", league.GameCode, "mlb")
+			}
+			start := time.Time(league.StartDate).Format("2006-02-01")
+			if start != "2016-03-04" {
+				t.Errorf("League unmarshaled incorrectley. StartDate: %s, expected %s", start, "2016-03-04")
+			}
+		},
+	}
+}
+
+func getUserSingleMetaTestSet(t *testing.T) queryTest {
+	q := QueryBuilder{UserQB: &fantasy.UserQueryBuilder{ActiveUser: true}}
+	url := q.Url()
+
+	return queryTest{
+		qb:     &q,
+		client: getXMLClient(url, "user-leagues-meta.xml", t),
+		want:   1,
+	}
+}
+
+func getXMLClient(url, filename string, t *testing.T) *gotest.RegisteredClient {
+	client := gotest.NewRegisteredClient()
+
 	// test valid xml
-	file, err := os.Open("test/single-league-meta.xml")
+	file, err := os.Open("test/" + filename)
 	if err != nil {
-		t.Error("Could not open test/single-league-meta.xml", err)
+		t.Error("Could not open test/"+filename, err)
 	}
 	xml, err := ioutil.ReadAll(file)
 	if err != nil {
-		t.Error("Could not read test/single-league-meta.xml", err)
+		t.Error("Could not read test/"+filename, err)
 	}
 
 	headers := make(map[string]string)
 	headers["Content-Type"] = "text/xml"
 	client.Register(url, "get", gotest.NewSimpleRoundTrip(xml, headers))
-	leagues, err := q.Get(client.Client)
-	if err != nil {
-		t.Errorf("Unexpected QueryBuilder.Get error: %s", err)
-	}
-	if len(leagues) != 1 {
-		t.Errorf("Unexpected League len got %d, expected %d", len(leagues), 1)
 
-	}
-
-	league := leagues[0]
-
-	if league.GameCode != "mlb" {
-		t.Errorf("League unmarshaled incorrectley. GameCode: %s, expected %s", league.GameCode, "mlb")
-	}
+	return client
 }
